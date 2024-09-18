f6bc0665eebbe9ebb2d0c15a6c6d5551d851c7e0
Lucas Pena <lucaspenatorresdev@gmail.com>
2024-09-18 17:00:52 -0300

    feat(app): implement feature to list and mark goals as completed

--- src/main.ts
+++ src/main.ts
@@ -2,47 +2,49 @@ import { select } from '@inquirer/prompts'
 
 import { showMessage } from './utils'
 import { createGoal } from './features/create-goal'
+import { getListGoals } from './features/get-list-goals'
 
 async function start() {
   showMessage('Bem vindo(a) ao app de metas!')
 
-  const options = await select({
-    message: 'Menu:',
-    choices: [
-      {
-        name: 'Criar uma nova meta',
-        value: 'criar',
-      },
-      {
-        name: 'Listar metas',
-        value: 'listar',
-      },
-      {
-        name: 'Listar metas completadas',
-        value: 'completar',
-      },
-      {
-        name: 'Listar metas em aberto',
-        value: 'aberto',
-      },
-      {
-        name: 'Deletar meta(s)',
-        value: 'deletar',
-      },
-      {
-        name: 'Sair',
-        value: 'sair',
-      },
-    ],
-  })
-
   while (true) {
+    const options = await select({
+      message: 'Menu:',
+      choices: [
+        {
+          name: 'Criar uma nova meta',
+          value: 'criar',
+        },
+        {
+          name: 'Listar metas',
+          value: 'listar',
+        },
+        {
+          name: 'Listar metas completadas',
+          value: 'completar',
+        },
+        {
+          name: 'Listar metas em aberto',
+          value: 'aberto',
+        },
+        {
+          name: 'Deletar meta(s)',
+          value: 'deletar',
+        },
+        {
+          name: 'Sair',
+          value: 'sair',
+        },
+      ],
+    })
+
     switch (options) {
       case 'criar':
         await createGoal()
         break
 
       case 'listar':
+        await getListGoals()
         break
 
       case 'completar':
