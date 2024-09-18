12428e40296a0b77a782d2bcb44dbdbee996a4ae
Lucas Pena <lucaspenatorresdev@gmail.com>
2024-09-18 14:59:24 -0300

    feat(db): add functions to manage goals data

---
+++ src/db/index.ts
@@ -0,0 +1,36 @@
+import fs from 'node:fs/promises'
+
+export type Goal = {
+  value: string
+  isChecked: string
+}
+
+export let goals: Goal[] = []
+
+export async function loadingGoals() {
+  try {
+    const data = await fs.readFile('./src/db/goals.json', 'utf-8')
+    goals = JSON.parse(data)
+
+    return
+  } catch (error) {
+    goals = []
+
+    return
+  }
+}
+
+export async function saveGoal() {
+  await fs.writeFile('./src/db/goals.json', JSON.stringify(goals, null, 1))
+  return
+}
+
+export function deleteItems(itemsToDelete: string[]) {
+  itemsToDelete.forEach(item => {
+    goals = goals.filter(goal => {
+      return goal.value !== item
+    })
+  })
+
+  return
+}
